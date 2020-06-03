5c58322edad90ad3a3e47753108d2c1fa1f56731
Tunechi-chris <66219578+Tunechi-chris@users.noreply.github.com>
2020-06-03 08:19:59 -0700

    Add files via upload

---
+++ Tunechi.js
@@ -0,0 +1,6 @@
+let person = {
+    Name:'Mudalue Emeka',
+    log : 'HNG-01029',
+    language: 'javascript'
+};
+console.log(JSON.stringify((`Hello world, this is ${person.Name} with HNGi7 ID${person.log} using ${person.language} for stage 2 task`)));
